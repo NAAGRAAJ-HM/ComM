3ede6200ae10a1e6cdc100d1440f0430c9ab0c8d
root <nagaraja.hm.0001@gmail.com>
2022-01-06 22:44:39 +0100

    Update: submodule ComM

--- LATEST/ComM.cpp
+++ LATEST/ComM.cpp
@@ -5,3 +5,51 @@ class_ComM ComM;
 void class_ComM::InitFunction(void){
 }
 
+void class_ComM::DeInitFunction(void){
+}
+
+void class_ComM::GetState(void){
+}
+
+void class_ComM::GetStatus(void){
+}
+
+void class_ComM::GetInhibitionStatus(void){
+}
+
+void class_ComM::RequestComMode(void){
+}
+
+void class_ComM::GetMaxComMode(void){
+}
+
+void class_ComM::GetRequestedComMode(void){
+}
+
+void class_ComM::GetCurrentComMode(void){
+}
+
+void class_ComM::PreventWakeUp(void){
+}
+
+void class_ComM::LimitChannelToNoComMode(void){
+}
+
+void class_ComM::LimitECUToNoComMode(void){
+}
+
+void class_ComM::ReadInhibitCounter(void){
+}
+
+void class_ComM::ResetInhibitCounter(void){
+}
+
+void class_ComM::SetECUGroupClassification(void){
+}
+
+void class_ComM::GetVersionInfo(void){
+}
+
+void class_ComM::MainFunction(void){
+}
+
--- LATEST/ComM.h
+++ LATEST/ComM.h
@@ -2,7 +2,23 @@
 
 class class_ComM{
    public:
-      void InitFunction(void);
+      void InitFunction              (void);
+      void DeInitFunction            (void);
+      void GetState                  (void);
+      void GetStatus                 (void);
+      void GetInhibitionStatus       (void);
+      void RequestComMode            (void);
+      void GetMaxComMode             (void);
+      void GetRequestedComMode       (void);
+      void GetCurrentComMode         (void);
+      void PreventWakeUp             (void);
+      void LimitChannelToNoComMode   (void);
+      void LimitECUToNoComMode       (void);
+      void ReadInhibitCounter        (void);
+      void ResetInhibitCounter       (void);
+      void SetECUGroupClassification (void);
+      void GetVersionInfo            (void);
+      void MainFunction              (void);
 };
 
 extern class_ComM ComM;
