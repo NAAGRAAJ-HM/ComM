4f39ac7f8419c74dea1b8d011ec0bfad7e247282
root <nagaraja.hm.0001@gmail.com>
2022-01-12 09:14:35 +0100

    Update: submodule ComM

--- LATEST/ComM.cpp
+++ LATEST/ComM.cpp
@@ -2,54 +2,54 @@
 
 class_ComM ComM;
 
-void class_ComM::InitFunction(void){
+FUNC(void, COMM_CODE) class_ComM::InitFunction(void){
 }
 
-void class_ComM::DeInitFunction(void){
+FUNC(void, COMM_CODE) class_ComM::DeInitFunction(void){
 }
 
-void class_ComM::GetState(void){
+FUNC(void, COMM_CODE) class_ComM::GetState(void){
 }
 
-void class_ComM::GetStatus(void){
+FUNC(void, COMM_CODE) class_ComM::GetStatus(void){
 }
 
-void class_ComM::GetInhibitionStatus(void){
+FUNC(void, COMM_CODE) class_ComM::GetInhibitionStatus(void){
 }
 
-void class_ComM::RequestComMode(void){
+FUNC(void, COMM_CODE) class_ComM::RequestComMode(void){
 }
 
-void class_ComM::GetMaxComMode(void){
+FUNC(void, COMM_CODE) class_ComM::GetMaxComMode(void){
 }
 
-void class_ComM::GetRequestedComMode(void){
+FUNC(void, COMM_CODE) class_ComM::GetRequestedComMode(void){
 }
 
-void class_ComM::GetCurrentComMode(void){
+FUNC(void, COMM_CODE) class_ComM::GetCurrentComMode(void){
 }
 
-void class_ComM::PreventWakeUp(void){
+FUNC(void, COMM_CODE) class_ComM::PreventWakeUp(void){
 }
 
-void class_ComM::LimitChannelToNoComMode(void){
+FUNC(void, COMM_CODE) class_ComM::LimitChannelToNoComMode(void){
 }
 
-void class_ComM::LimitECUToNoComMode(void){
+FUNC(void, COMM_CODE) class_ComM::LimitECUToNoComMode(void){
 }
 
-void class_ComM::ReadInhibitCounter(void){
+FUNC(void, COMM_CODE) class_ComM::ReadInhibitCounter(void){
 }
 
-void class_ComM::ResetInhibitCounter(void){
+FUNC(void, COMM_CODE) class_ComM::ResetInhibitCounter(void){
 }
 
-void class_ComM::SetECUGroupClassification(void){
+FUNC(void, COMM_CODE) class_ComM::SetECUGroupClassification(void){
 }
 
-void class_ComM::GetVersionInfo(void){
+FUNC(void, COMM_CODE) class_ComM::GetVersionInfo(void){
 }
 
-void class_ComM::MainFunction(void){
+FUNC(void, COMM_CODE) class_ComM::MainFunction(void){
 }
 
--- LATEST/ComM.h
+++ LATEST/ComM.h
@@ -1,26 +1,27 @@
 #pragma once
 
 #include "Std_Types.h"
+#include "Compiler_Cfg_ComM.h"
 
 class class_ComM{
    public:
-      void InitFunction              (void);
-      void DeInitFunction            (void);
-      void GetState                  (void);
-      void GetStatus                 (void);
-      void GetInhibitionStatus       (void);
-      void RequestComMode            (void);
-      void GetMaxComMode             (void);
-      void GetRequestedComMode       (void);
-      void GetCurrentComMode         (void);
-      void PreventWakeUp             (void);
-      void LimitChannelToNoComMode   (void);
-      void LimitECUToNoComMode       (void);
-      void ReadInhibitCounter        (void);
-      void ResetInhibitCounter       (void);
-      void SetECUGroupClassification (void);
-      void GetVersionInfo            (void);
-      void MainFunction              (void);
+      FUNC(void, COMM_CODE) InitFunction              (void);
+      FUNC(void, COMM_CODE) DeInitFunction            (void);
+      FUNC(void, COMM_CODE) GetState                  (void);
+      FUNC(void, COMM_CODE) GetStatus                 (void);
+      FUNC(void, COMM_CODE) GetInhibitionStatus       (void);
+      FUNC(void, COMM_CODE) RequestComMode            (void);
+      FUNC(void, COMM_CODE) GetMaxComMode             (void);
+      FUNC(void, COMM_CODE) GetRequestedComMode       (void);
+      FUNC(void, COMM_CODE) GetCurrentComMode         (void);
+      FUNC(void, COMM_CODE) PreventWakeUp             (void);
+      FUNC(void, COMM_CODE) LimitChannelToNoComMode   (void);
+      FUNC(void, COMM_CODE) LimitECUToNoComMode       (void);
+      FUNC(void, COMM_CODE) ReadInhibitCounter        (void);
+      FUNC(void, COMM_CODE) ResetInhibitCounter       (void);
+      FUNC(void, COMM_CODE) SetECUGroupClassification (void);
+      FUNC(void, COMM_CODE) GetVersionInfo            (void);
+      FUNC(void, COMM_CODE) MainFunction              (void);
 };
 
 extern class_ComM ComM;
