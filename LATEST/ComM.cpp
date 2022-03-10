52719cce6b5fcd917e3a83207b425ec90663f37d
root <nagaraja.hm.0001@gmail.com>
2022-03-10 12:49:36 +0100

    Update: AUTOSAR basic software service module ComM

--- LATEST/ComM.cpp
+++ LATEST/ComM.cpp
@@ -7,8 +7,8 @@
 /* #INCLUDES                                         */
 /*****************************************************/
 #include "module.h"
-#include "ComM_EcuM.h"
-#include "ComM_SchM.h"
+#include "infComM_EcuM.h"
+#include "infComM_SchM.h"
 #include "ComM_Unused.h"
 
 /*****************************************************/
@@ -28,6 +28,7 @@ class module_ComM:
    public:
       FUNC(void, COMM_CODE) InitFunction   (void);
       FUNC(void, COMM_CODE) DeInitFunction (void);
+      FUNC(void, COMM_CODE) GetVersionInfo (void);
       FUNC(void, COMM_CODE) MainFunction   (void);
 };
 
@@ -44,6 +45,7 @@ class module_ComM:
 /*****************************************************/
 module_ComM    ComM;
 infEcuMClient* gptrinfEcuMClient_ComM = &ComM;
+infDcmClient*  gptrinfDcmClient_ComM  = &ComM;
 infSchMClient* gptrinfSchMClient_ComM = &ComM;
 
 /*****************************************************/
@@ -53,6 +55,9 @@ FUNC(void, COMM_CODE) module_ComM::InitFunction(void){
 }
 
 FUNC(void, COMM_CODE) module_ComM::DeInitFunction(void){
+}
+
+FUNC(void, COMM_CODE) module_ComM::GetVersionInfo(void){
 }
 
 FUNC(void, COMM_CODE) module_ComM::MainFunction(void){
