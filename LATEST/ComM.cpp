adaf724c8e7893ec6a89818672a8a24a1b11b235
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:47:51 +0200

    Update: AUTOSAR basic software service module ComM

--- LATEST/ComM.cpp
+++ LATEST/ComM.cpp
@@ -90,7 +90,22 @@ FUNC(void, COMM_CODE) module_ComM::GetVersionInfo(void){
 FUNC(void, COMM_CODE) module_ComM::MainFunction(void){
 }
 
-#include "ComM_Unused.hpp"
+class class_ComM_Unused{
+   public:
+      FUNC(void, COMM_CODE) GetState                  (void);
+      FUNC(void, COMM_CODE) GetStatus                 (void);
+      FUNC(void, COMM_CODE) GetInhibitionStatus       (void);
+      FUNC(void, COMM_CODE) RequestComMode            (void);
+      FUNC(void, COMM_CODE) GetMaxComMode             (void);
+      FUNC(void, COMM_CODE) GetRequestedComMode       (void);
+      FUNC(void, COMM_CODE) GetCurrentComMode         (void);
+      FUNC(void, COMM_CODE) PreventWakeUp             (void);
+      FUNC(void, COMM_CODE) LimitChannelToNoComMode   (void);
+      FUNC(void, COMM_CODE) LimitECUToNoComMode       (void);
+      FUNC(void, COMM_CODE) ReadInhibitCounter        (void);
+      FUNC(void, COMM_CODE) ResetInhibitCounter       (void);
+      FUNC(void, COMM_CODE) SetECUGroupClassification (void);
+};
 
 FUNC(void, COMM_CODE) class_ComM_Unused::GetState(void){
 }
