d13bdad306e9c337bfaa70c991e1b451a8fbc6c5
root <nagaraja.hm.0001@gmail.com>
2022-03-30 12:43:02 +0200

    Update: AUTOSAR basic software service module ComM

--- LATEST/ComM.cpp
+++ LATEST/ComM.cpp
@@ -6,7 +6,7 @@
 /******************************************************************************/
 /* #INCLUDES                                                                  */
 /******************************************************************************/
-#include "module.hpp"
+#include "Module.hpp"
 #include "infComM_EcuM.hpp"
 #include "infComM_Dcm.hpp"
 #include "infComM_SchM.hpp"
@@ -37,6 +37,9 @@ class module_ComM:
    public:
       module_ComM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
+      FUNC(void, _CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      );
       FUNC(void, COMM_CODE) InitFunction   (void);
       FUNC(void, COMM_CODE) DeInitFunction (void);
       FUNC(void, COMM_CODE) MainFunction   (void);
@@ -73,7 +76,19 @@ VAR(module_ComM, COMM_VAR) ComM(
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
-FUNC(void, COMM_CODE) module_ComM::InitFunction(void){
+FUNC(void, COMM_CODE) module_ComM::InitFunction(
+   CONSTP2CONST(CfgComM_Type, CFGCOMM_CONFIG_DATA, CFGCOMM_APPL_CONST) lptrCfgComM
+){
+   if(NULL_PTR == lptrCfgComM){
+#if(STD_ON == ComM_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+// check lptrCfgComM for memory faults
+// use PBcfg_ComM as back-up configuration
+   }
    ComM.IsInitDone = E_OK;
 }
 
