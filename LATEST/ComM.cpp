def5103d8b04af6836d3c0e2dc8d87af6e58f86c
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-03 10:08:53 +0000

    Update: AUTOSAR basic software service module ComM

--- LATEST/ComM.cpp
+++ LATEST/ComM.cpp
@@ -82,79 +82,79 @@ FUNC(void, COMM_CODE) module_ComM::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, COMM_CONFIG_DATA, COMM_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == ComM_InitCheck)
-   if(E_OK == IsInitDone){
-#if(STD_ON == ComM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK != IsInitDone){
 #endif
-   }
-   else{
-#endif
-      if(NULL_PTR == lptrCfgModule){
-#if(STD_ON == ComM_DevErrorDetect)
-         Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-         );
-#endif
-      }
-      else{
+      if(NULL_PTR != lptrCfgModule){
          if(STD_LOW){
-// check lptrCfgModule for memory faults
+            // check lptrCfgModule for memory faults
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfgCanIf as back-up configuration
+            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgComM;
          }
       }
-      IsInitDone = E_OK;
+      else{
+#if(STD_ON == ComM_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
 #if(STD_ON == ComM_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == ComM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
 
 FUNC(void, COMM_CODE) module_ComM::DeInitFunction(void){
 #if(STD_ON == ComM_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == ComM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK == IsInitDone){
 #endif
+#if(STD_ON == ComM_InitCheck)
+      IsInitDone = E_NOT_OK;
    }
    else{
+#if(STD_ON == ComM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
 #endif
-      IsInitDone = E_NOT_OK;
-#if(STD_ON == ComM_InitCheck)
    }
 #endif
 }
 
 FUNC(void, COMM_CODE) module_ComM::MainFunction(void){
 #if(STD_ON == ComM_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == ComM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
-#endif
-   }
-   else{
+   if(E_OK == IsInitDone){
 #endif
 #if(STD_ON == ComM_InitCheck)
+   }
+   else{
+#if(STD_ON == ComM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
