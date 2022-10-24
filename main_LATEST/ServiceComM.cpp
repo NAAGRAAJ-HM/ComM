f7b9598874d65182c2e7e498935c19256f2eecaa
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-10-24 22:03:56 +0200

    Update: AUTOSAR basic software service module for Com manager

--- main_LATEST/ServiceComM.cpp
+++ main_LATEST/ServiceComM.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* File   : ServiceComM.cpp                                                          */
+/* File   : ServiceComM.cpp                                                   */
 /* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
 /******************************************************************************/
 
@@ -13,8 +13,8 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define SERVICECOMM_AR_RELEASE_VERSION_MAJOR                                          4
-#define SERVICECOMM_AR_RELEASE_VERSION_MINOR                                          3
+#define SERVICECOMM_AR_RELEASE_VERSION_MAJOR                                   4
+#define SERVICECOMM_AR_RELEASE_VERSION_MINOR                                   3
 
 /******************************************************************************/
 /* MACROS                                                                     */
