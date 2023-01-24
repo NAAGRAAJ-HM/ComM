ca71566f7337e8fadd4fc6d8e79a36b8afbba471
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:51:07 +0100

    Update: AUTOSAR basic software service module for Com manager

---
+++ main_R2023/infServiceComM_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceComM_ServiceNvM.hpp                                     */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICECOMM_AR_RELEASE_VERSION_MAJOR                              4
+#define CONSTSERVICECOMM_AR_RELEASE_VERSION_MINOR                              3
+
+#define Const_DefaultInit_ConstServiceComM                                                                               \
+   ,  {CONSTSERVICECOMM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICECOMM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
