6af1cc12d16ec0703469bbcc6d519f1c11ff7f7e
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-02-04 19:29:58 +0100

    Update: AUTOSAR basic software service module for Com manager

--- main_R2023/infServiceComM_ServiceNvM_Types.hpp
+++ main_R2023/infServiceComM_ServiceNvM_Types.hpp
@@ -10,9 +10,11 @@
 class NvM_BlocksRom_ServiceComM_Type:
       public ConstModule_TypeAbstract
 {
-/******************************************************************************/
-/* CONSTS                                                                     */
-/******************************************************************************/
+};
+
+class NvM_BlocksNv_ServiceComM_Type:
+      public CfgModule_TypeAbstract
+{
 };
 
 /******************************************************************************/
